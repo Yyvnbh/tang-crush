53644d98768665d477b2f1f65f699e30c7aba1d4
anhdung-anh <85271000+anhdung-anh@users.noreply.github.com>
2021-06-03 12:05:17 +0700

    Create config.js

--- config.js
+++ config.js
@@ -2,16 +2,16 @@ const CONFIG = {
     introTitle: 'Bông Hâm à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Lạc vào tim anh lẻ loi đây nekk
+    Đằng sau chữ YÊU đây là THƯƠNG nhiều lắm`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Tạm chấp nhận, Vậy nhá <33',
+    btnNo: 'Không,không bao giờ :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu mình Bông <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thương nhiều lắm luôn đấy<<<3',
     mess: 'Anh biết mà 🥰. Yêu Bông nhiều lắm luôn😘😘',
     messDesc: 'Mấy nữa anh đón đi chơi với bông nha.',
     btnAccept: 'Okiiiii lun <3',
