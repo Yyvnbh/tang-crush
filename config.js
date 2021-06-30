61ede8952d299df8b4af1c62950590ad69d817b3
Yyvnbh <86708999+Yyvnbh@users.noreply.github.com>
2021-06-30 14:35:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const  CONFIG  = {
     introTitle : Này TUýt
-    introDesc : Thường       lệ       vốn       trái     dẤt
-    Mà  sao  em  cứ  đi  nhầm  đường
-    Lạc vào tim anh lẻ loi
+    introDesc :Trái đất vốn là thường
+    Mà  sao  Tuýt  cứ  đi  nhầm  đường
+    Lạc vào tim Quang lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnIntro: 'hehe',
+    title: 'Phải chăng Tuýt đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Tuýt đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích Quang <33',
+    btnNo: 'Không, Anh Quang à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Tuýt lại yêu Quang <3',
+    btnReply: 'Gửi cho Quang <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Anh biết mà:)). Yêu Tuýt🤣',
+    messDesc: Đấm Nhaooo Khongg Tuýt:>>.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
